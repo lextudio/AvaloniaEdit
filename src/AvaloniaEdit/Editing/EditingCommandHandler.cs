acabfbcf8646a837e5b9d29b1d71c1ab96e53074
Lex Li <support@lextm.com>
2025-12-08 14:18:41 -0500

    Refactor segment deletion to use Enumerable.Reverse for improved readability

--- src/AvaloniaEdit/Editing/EditingCommandHandler.cs
+++ src/AvaloniaEdit/Editing/EditingCommandHandler.cs
@@ -198,7 +198,7 @@ namespace AvaloniaEdit.Editing
                     {
                         foreach (var segment in segments.Reverse())
                         {
-                            foreach (var writableSegment in textArea.GetDeletableSegments(segment).Reverse())
+                            foreach (var writableSegment in Enumerable.Reverse(textArea.GetDeletableSegments(segment)))
                             {
                                 transformSegment(textArea, writableSegment);
                             }
